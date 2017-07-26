93d612cd104640f333b2669c35cfbf3cf03ca37c
julio9319 <juliohbk_93@hotmail.es>
2017-07-26 03:45:08 -0500

    configuracion con la base de MYSQL y añadiendo dependencias para correr servidor django

--- YuGiOhGYEProyecto2/settings.py
+++ YuGiOhGYEProyecto2/settings.py
@@ -75,9 +75,13 @@ WSGI_APPLICATION = 'YuGiOhGYEProyecto2.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+         'ENGINE': 'django.db.backends.mysql',
+         'NAME': 'yugiohgye',
+         'USER': 'root',
+         'PASSWORD': 'eltipo',
+         'HOST': '127.0.0.1',   # Or an IP Address that your DB is hosted on
+         'PORT': '3306',
+     },
 }
 
 
