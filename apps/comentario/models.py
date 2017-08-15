4e41c3ac1df8f753c0c6b8d7feda027ab1061af4
Alex Ferrin <alex-710@hotmail.com>
2017-08-14 19:40:50 -0500

    Clave foranea faltaba

--- apps/comentario/models.py
+++ apps/comentario/models.py
@@ -6,15 +6,6 @@ from apps.ygoapp.models import Usuario
 
 # Create your models here.
 
-class Comentario(models.Model):
-    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
-    contenido = models.TextField()
-    fecha = models.DateField()
-    hora = models.TimeField()
-    
-    class Meta:
-        db_table = 'comentario'
-
 class Tema(models.Model):
     usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
     descripcion = models.CharField(max_length=60)
@@ -22,3 +13,15 @@ class Tema(models.Model):
 
     class Meta:
         db_table = 'tema'
+
+class Comentario(models.Model):
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    contenido = models.TextField()
+    fecha = models.DateField()
+    hora = models.TimeField()
+    tema = models.ForeignKey(Tema, on_delete=models.CASCADE)
+    
+    class Meta:
+        db_table = 'comentario'
+
+
