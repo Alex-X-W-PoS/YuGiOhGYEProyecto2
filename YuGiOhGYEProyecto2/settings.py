4032bf7231c5bacce151fd2d121a0044e4284c32
TaiEspol <taiyuchen92286@gmail.com>
2017-09-03 23:19:23 -0500

    por si acaso

--- YuGiOhGYEProyecto2/settings.py
+++ YuGiOhGYEProyecto2/settings.py
@@ -90,7 +90,7 @@ DATABASES = {
          'ENGINE': 'django.db.backends.mysql',
          'NAME': 'yugiohgye',
          'USER': 'root',
-         'PASSWORD': 'eltipo',
+         'PASSWORD': '',
          'HOST': '127.0.0.1',   # Or an IP Address that your DB is hosted on
          'PORT': '3306',
      },
